333c4ce5394d7db32c5bd64cbbca0574d145be9a
Justin_Ma <justin.ma@roborn.com>
2022-11-17 12:32:44 +0800

    feat: added script to auto gen yaml and move files

--- utils/custom_utils.py
+++ utils/custom_utils.py
@@ -2,22 +2,23 @@ import os
 import logging
 import sys
 import glob
-from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-logging.basicConfig(
-    format="%(asctime)s | %(levelname)s: %(message)s",
-    level=int(config["VERBOSE"]),
-    stream=sys.stdout,
-)
+
 
 
 def get_folder_files(path=r"data/images", extension="*.jpg", reverse=True):
     path = path + "/" + extension
     list_of_files = glob.glob(path)  # * means all if need specific format then *.csv
     list_of_files.sort(key=os.path.getmtime, reverse=reverse)
-
     return list_of_files
+
+"""
+Checks if the folder exits and creates it if it does not
+"""
+def create_folder(directory_path: str) -> bool:
+  if (not os.path.exists(directory_path)):
+      os.makedirs(directory_path)
+  return os.path.normpath(directory_path)
 
 
 if __name__ == "__main__":
